827541f501be64ead6b7194726e659e22d894da1
Nikolai Madridano <nikol_m18@yahoo.com>
2020-09-07 08:51:59 -0700

    overview update #1

--- src/layout/Overview/Overview.tsx
+++ src/layout/Overview/Overview.tsx
@@ -2,40 +2,41 @@ import React, { useEffect, useState } from 'react'
 import ProductItem from '../../components/Cards/Item';
 import axios from 'axios';
 
-
 interface itemsProps {
     loading: boolean, 
     data: []
 }
 
 function Overview(props: any): JSX.Element  {
-   
+
     const [items, setItems] = useState<itemsProps>({
         loading: true,
         data: []
     });
 
     useEffect(() => {
-       const get = async () => {
-           const response = await axios.get('/items.json')
-                .then(res => res.data)
-                .catch(err => console.log(err))
-                    setItems({
-                        loading: false,
-                        data: response
-                    });
-       }
-       get()
+        axios.get('/items.json')
+            .then(res => {
+                setItems({
+                    loading: false,
+                    data: res.data
+                })
+            })
+            .catch(err => console.log(err))
     }, []);
 
     const selectedItem: any =  items.data.find((x: any)  => {
         return x.id === props.match.params.id;
     })
-    console.log(selectedItem)
     return ( 
         <div>
             <h1>OVERVIEW </h1>
-            <img src={`/${selectedItem.content.others.photoUrl}`} className="img-fluid" alt=""/>
+            { items.loading 
+                ? <h1>Page is loading</h1>
+                : <img 
+                    src={`/${selectedItem.content.others.photoUrl}`} 
+                    className="img-fluid" alt=""/>
+            }
         </div>
      );
 }
